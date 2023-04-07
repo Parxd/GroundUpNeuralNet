1466de74a508d6cb8660f3179c44318c62648441
parxed <parxed>
2023-04-07 17:24:52 -0500

    reorg

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parxed/code/projects/GroundUpNeuralNet
+# Build directory: /home/parxed/code/projects/GroundUpNeuralNet/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

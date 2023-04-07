1b55539eca9fb0369ed05626307e0c135d0562b9
parxed <parxed>
2023-04-07 16:55:16 -0500

    init commit

---
+++ build/CMakeFiles/GroundUpNeuralNet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GroundUpNeuralNet.dir/main.cpp.o"
+  "CMakeFiles/GroundUpNeuralNet.dir/main.cpp.o.d"
+  "GroundUpNeuralNet"
+  "GroundUpNeuralNet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GroundUpNeuralNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

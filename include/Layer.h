84b477f81642032dc5d3a5a29ffbfbfb2e9bd7ef
parxed <parxed>
2023-04-09 20:53:19 -0500

    implement Layer

--- include/Layer.h
+++ include/Layer.h
@@ -10,18 +10,33 @@ public:
     ~Layer() = default;
     /**
      * @brief Feedforward method of a linear layer class
-     * @param [input] The matrix that is fed into this layer from the previous layer 
-     * @param [output] 
+     * @param [input] The input matrix that is fed into this layer from the previous layer 
+     * @param [output] The output matrix after operations w/ weights & biases
+     * @return [NONE] Modifies output matrix in-place
     */
     void forward(const Eigen::MatrixXf& input, Eigen::MatrixXf& output) override;
-    void backward(const Eigen::MatrixXf& dout, Eigen::MatrixXf& ddout) override;
+    /**
+     * @brief Backpropagation method of a linear layer class
+     * @param [dEW] Derivative of error with respect to weight
+     * @param [output] The output matrix after operation with updated weights
+     * @return [NONE] Modifies output matrix (& weights/biases) in-place
+    */
+    void backward(const Eigen::MatrixXf& dEW, Eigen::MatrixXf& output) override;
+
+    /**
+     * @brief Set learning rate of this individual layer
+     * @param [learningRate] Desired learning rate
+     * @return [NONE]
+    */
+    void setLearningRate(const float& learningRate);
 
 private:
-    Eigen::VectorXf activationInputs;
+    Eigen::VectorXf inputs;
     Eigen::MatrixXf weights;
     Eigen::MatrixXf bias;
     int inputFeatures;
     int outputFeatures;
+    float eta;
 };
 
 #endif
--- src/Layer.cpp
+++ src/Layer.cpp
@@ -3,17 +3,25 @@
 Layer::Layer(int numInputs, int numOutputs):
     inputFeatures(numInputs), outputFeatures(numOutputs)
 {
-    activationInputs = Eigen::VectorXf(inputFeatures);
     weights = Eigen::MatrixXf::Random(inputFeatures, outputFeatures);
     bias = Eigen::MatrixXf::Random(1, outputFeatures);
 }
 
-void forward(const Eigen::MatrixXf& input, Eigen::MatrixXf& output)
+void Layer::forward(const Eigen::MatrixXf& input, Eigen::MatrixXf& output)
 {
-
+    inputs = input;
+    output = input.matrix() * weights.matrix();
+    output += bias;
 }
 
-void backward(const Eigen::MatrixXf& dout, Eigen::MatrixXf& ddout)
+void Layer::backward(const Eigen::MatrixXf& dEW, Eigen::MatrixXf& output)
 {
+    weights = weights - (eta * (inputs.transpose() * dEW));
+    bias = bias - (eta * output);
+    output = dEW * weights.transpose();
+}
 
+void Layer::setLearningRate(const float& learningRate)
+{
+    eta = learningRate;
 }

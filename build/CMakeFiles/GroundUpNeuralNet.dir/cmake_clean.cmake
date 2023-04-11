bbe108887cd972247edc314c512fa8240607b00e
parxed <102764808+Parxd@users.noreply.github.com>
2023-04-11 00:51:24 -0500

    configure cmake

---
+++ build/CMakeFiles/GroundUpNeuralNet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GroundUpNeuralNet.dir/src/Layer.cpp.o"
+  "CMakeFiles/GroundUpNeuralNet.dir/src/Layer.cpp.o.d"
+  "CMakeFiles/GroundUpNeuralNet.dir/tests/main.cpp.o"
+  "CMakeFiles/GroundUpNeuralNet.dir/tests/main.cpp.o.d"
+  "GroundUpNeuralNet"
+  "GroundUpNeuralNet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GroundUpNeuralNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GroundUpNeuralNet.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GroundUpNeuralNet.
+# This may be replaced when dependencies are built.

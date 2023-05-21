6e6bdb67958226bc1ba3cecc32f61391dd6b100c
parxed <102764808+Parxd@users.noreply.github.com>
2023-05-20 21:27:06 -0700

    Implement templatization for loss function used by trainer, delete Trainer.cpp for this reason

--- include/containers/Trainer.h
+++ include/containers/Trainer.h
@@ -3,18 +3,36 @@
 
 #include "../containers/Container.h"
 
+template <typename LossFunction>
 class Trainer {
 public:
     Trainer() = delete;
 
     ~Trainer() = delete;
 
-    static void train(Container& model,
+    static inline void train(Container& model,
                       Eigen::MatrixXf& trainData,
                       Eigen::MatrixXf& trainLabels,
                       int batchSize,
                       int epochs,
-                      int printFreq);
+                      int printFreq) {
+
+        int numBatches = int(trainData.cols()) / batchSize;
+        for (int i = 0; i < epochs; ++i) {
+            float loss = 0.f;
+            for (int j = 0; j < numBatches; ++j) {
+                auto batchData = trainData.block(0, j * batchSize, trainData.rows(), batchSize);
+                auto batchLabels = trainLabels.block(0, j * batchSize, trainData.rows(), batchSize);
+                auto forward = model.forward(batchData);
+                loss += LossFunction::forward(forward, batchLabels);
+                model.backward(LossFunction::backward(forward, batchLabels));
+            }
+            loss /= float(numBatches);
+            if (i % printFreq == 0) {
+                std::cout << "Epoch " << i + 1 << " loss: " << loss << std::endl;
+            }
+        }
+    }
 };
 
 #endif //GROUNDUPNEURALNET_TRAINER_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,17 @@
 
 int main(int argc, char** argv)
 {
+    setvbuf(stdout, nullptr, _IONBF, 0);
     Container cont(
             new Linear(2, 10),
             new ReLU(),
             new Linear(10, 10),
-            new ReLU(),
-            new Linear(10, 2),
-            new Sigmoid()
+            new Sigmoid(),
+            new Linear(10, 2)
             );
     auto data = Sine::generate(500000, 0.2, 10, 5, 1.1);
     Eigen::MatrixXf features = data.topRows(2);
     Eigen::MatrixXf labels = data.bottomRows(2);
-    // In labels: top row 1 means that it is a correct sine point, bottom row 1 means
-    //            that it is an incorrect sine point
-    Trainer::train(cont, features, labels, 32, 10, 1);
+    Trainer<CE>::train(cont, features, labels, 32, 10, 1);
     return 0;
 }
